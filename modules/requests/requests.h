6d57e3688609969a3a80827c1f403ebb4f9f3095
Martin Schulz <schulzm@llnl.gov>
2010-11-29 18:39:27 -0800

    added new macro to allow access to the request envolope before it is being reported back through status

--- modules/requests/requests.h
+++ modules/requests/requests.h
@@ -1,36 +1,3 @@
-/*
-Copyright (c) 2008
-Lawrence Livermore National Security, LLC. 
-
-Produced at the Lawrence Livermore National Laboratory. 
-Written by Martin Schulz, schulzm@llnl.gov.
-LLNL-CODE-402774,
-All rights reserved.
-
-This file is part of P^nMPI. 
-
-Please also read the file "LICENSE" included in this package for 
-Our Notice and GNU Lesser General Public License.
-
-This program is free software; you can redistribute it and/or 
-modify it under the terms of the GNU General Public License 
-(as published by the Free Software Foundation) version 2.1 
-dated February 1999.
-
-This program is distributed in the hope that it will be useful, 
-but WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY 
-OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-terms and conditions of the GNU General Public License for more 
-details.
-
-You should have received a copy of the GNU Lesser General Public 
-License along with this program; if not, write to the 
-
-Free Software Foundation, Inc., 
-59 Temple Place, Suite 330, 
-Boston, MA 02111-1307 USA
-*/
-
 #include <mpi.h>
 #include <status.h>
 
@@ -94,6 +61,11 @@ typedef long MyReq_t;
   PNMPIMOD_Requests_Parameters_t* _store;\
   _store=fct(request);\
   (*((type*) (&(_store->data[offset]))))=val;\
+}
+
+#define REQ_ENVELOPE(request,fct,_env) \
+{\
+  _env=fct(request);\
 }
 
 #define REQ_FROM_STATUS(status,offset) REQ_FROM_STATUSARRAY(status,offset,0,1,0)
