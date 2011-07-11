46712f1446e4b8466a2f7548f7448f2dc7f2fd3a
Todd Gamblin <tgamblin@llnl.gov>
2011-07-11 08:36:44 -0700

    Changed cmake config file location from lib to share.

--- demo/simple.c
+++ demo/simple.c
@@ -36,37 +36,32 @@ Boston, MA 02111-1307 USA
 
 #include <mpi.h>
 
-int main(int argc, char **argv)
-{
-  int rank,size,i,buf[5],err;
+int main(int argc, char **argv) {
+  int rank, size, i, err;
+  int buf[5] = { 0, 0, 0, 0, 0 };
   MPI_Status status;
 
   err=MPI_Init(&argc,&argv);
-  if (err!=MPI_SUCCESS)
-    {
-      printf("ERROR IN INIT: %i\n",err);
-      exit(1);
-    }
-
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  MPI_Comm_size(MPI_COMM_WORLD,&size);
+  if (err!=MPI_SUCCESS) {
+    printf("ERROR IN INIT: %i\n",err);
+    exit(1);
+  }
+  
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   MPI_Barrier(MPI_COMM_WORLD);
 
-  if (rank==0)
-    {
-      for (i=1; i<size; i++)
-	{
-	  MPI_Recv(buf,5,MPI_INT,MPI_ANY_SOURCE,42,MPI_COMM_WORLD,&status);
-	  MPI_Send(buf,5,MPI_INT,status.MPI_SOURCE,42,MPI_COMM_WORLD);
-	  printf("GOT %i\n",status.MPI_SOURCE);
-	}
+  if (rank==0) {
+    for (i=1; i<size; i++) {
+      MPI_Recv(buf, 5, MPI_INT, MPI_ANY_SOURCE, 42, MPI_COMM_WORLD, &status);
+      MPI_Send(buf, 5, MPI_INT, status.MPI_SOURCE, 42, MPI_COMM_WORLD);
+      printf("GOT %i\n",status.MPI_SOURCE);
     }
-  else
-    {
-      MPI_Send(buf,5,MPI_INT,0,42,MPI_COMM_WORLD);
-      MPI_Recv(buf,5,MPI_INT,MPI_ANY_SOURCE,42,MPI_COMM_WORLD,&status);
-    }
+  } else {
+    MPI_Send(buf, 5, MPI_INT, 0, 42, MPI_COMM_WORLD);
+    MPI_Recv(buf, 5, MPI_INT, MPI_ANY_SOURCE, 42, MPI_COMM_WORLD, &status);
+  }
 
   MPI_Barrier(MPI_COMM_WORLD);
 
