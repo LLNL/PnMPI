4c11d0fae1dc00fe5ce90e9ef0ae436ebdfa94b9
Alexander Haase <alexander.haase@rwth-aachen.de>
2017-02-20 13:31:36 +0100

    Added testcases for getPcontrol service functions.

---
+++ tests/pnmpi/service/getPcontrol.c
@@ -0,0 +1,89 @@
+/* This file is part of P^nMPI.
+ *
+ * Copyright (c)
+ *  2008-2016 Lawrence Livermore National Laboratories, United States of America
+ *  2011-2016 ZIH, Technische Universitaet Dresden, Federal Republic of Germany
+ *  2013-2016 RWTH Aachen University, Federal Republic of Germany
+ *
+ *
+ * P^nMPI is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation version 2.1 dated February 1999.
+ *
+ * P^nMPI is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with P^nMPI; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   51 Franklin St, Fifth Floor
+ *   Boston, MA 02110, USA
+ *
+ *
+ * Written by Martin Schulz, schulzm@llnl.gov.
+ *
+ * LLNL-CODE-402774
+ */
+
+/* This test case will check, if the module is able to query arguments. There
+ * will be tests for querying the same and a different module for valid
+ * arguments and querying an unknown argument in this test file. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pnmpi/debug_io.h>
+#include <pnmpi/service.h>
+
+
+#ifndef TEST_MODULE
+#define TEST_MODULE 0
+#endif
+
+
+void PNMPI_RegistrationPoint()
+{
+  /* Only the first module in the stack should execute this test. Other handles
+   * will be used to store arguments only. */
+  int self;
+  if (PNMPI_Service_GetModuleSelf(&self) != PNMPI_SUCCESS)
+    pnmpi_error("Can't get module ID.\n");
+  if (self != 0)
+    return;
+
+  int buffer;
+  int ret = PNMPI_Service_GetPcontrol(TEST_MODULE, &buffer);
+  switch (ret)
+    {
+    case PNMPI_SUCCESS: printf("GetPcontrol: %d\n", buffer); break;
+    case PNMPI_NOMODULE:
+      pnmpi_warning("GetPcontrol: module not found\n");
+      break;
+
+    default: pnmpi_error("Unknown error: %d\n", ret); break;
+    }
+}
+
+
+/* CONFIGS: self other no_module
+ *
+ * MODTYPE: XMPI
+ *
+ * PNMPICONF: module @MODNAME@\n
+ * PNMPICONF: pcontrol on\n
+ * PNMPICONF: module @MODNAME@\n
+ * PNMPICONF: pcontrol off\n
+ *
+ * RUN: @PNMPIZE@ -m @CMAKE_CURRENT_BINARY_DIR@ -c @PNMPICONF@ @TESTBIN_MPI_C@
+ *
+ * PASS-self: GetPcontrol: 1
+ *
+ * COMPILE_FLAGS-other: -DTEST_MODULE=1
+ * PASS-other: GetPcontrol: 0
+ *
+ * COMPILE_FLAGS-no_module: -DTEST_MODULE=999
+ * PASS-no_module: GetPcontrol: module not found
+ */
---
+++ tests/pnmpi/service/getPcontrolSelf.c
@@ -0,0 +1,66 @@
+/* This file is part of P^nMPI.
+ *
+ * Copyright (c)
+ *  2008-2016 Lawrence Livermore National Laboratories, United States of America
+ *  2011-2016 ZIH, Technische Universitaet Dresden, Federal Republic of Germany
+ *  2013-2016 RWTH Aachen University, Federal Republic of Germany
+ *
+ *
+ * P^nMPI is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation version 2.1 dated February 1999.
+ *
+ * P^nMPI is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with P^nMPI; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   51 Franklin St, Fifth Floor
+ *   Boston, MA 02110, USA
+ *
+ *
+ * Written by Martin Schulz, schulzm@llnl.gov.
+ *
+ * LLNL-CODE-402774
+ */
+
+/* This test case will check, if the module is able to query arguments. There
+ * will be tests for querying the same and a different module for valid
+ * arguments and querying an unknown argument in this test file. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pnmpi/debug_io.h>
+#include <pnmpi/service.h>
+
+
+void PNMPI_RegistrationPoint()
+{
+  /* Only the first module in the stack should execute this test. Other handles
+   * will be used to store arguments only. */
+  int self;
+  if (PNMPI_Service_GetModuleSelf(&self) != PNMPI_SUCCESS)
+    pnmpi_error("Can't get module ID.\n");
+  if (self != 1)
+    return;
+
+  printf("GetPcontrolSelf: %d\n", PNMPI_Service_GetPcontrolSelf());
+}
+
+
+/* MODTYPE: XMPI
+ *
+ * PNMPICONF: module @MODNAME@\n
+ * PNMPICONF: pcontrol off\n
+ * PNMPICONF: module @MODNAME@\n
+ * PNMPICONF: pcontrol on\n
+ *
+ * RUN: @PNMPIZE@ -m @CMAKE_CURRENT_BINARY_DIR@ -c @PNMPICONF@ @TESTBIN_MPI_C@
+ *
+ * PASS: GetPcontrolSelf: 1
+ */
