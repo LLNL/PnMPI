3b00e1c7c7297590c77ac0efcfa41fba9dcff3e8
Alexander Haase <alexander.haase@rwth-aachen.de>
2018-06-03 22:09:13 +0200

    Remove unused feature checks

--- cmakemodules/FeatureTests.cmake
+++ cmakemodules/FeatureTests.cmake
@@ -27,19 +27,9 @@
 #
 # LLNL-CODE-402774
 
-include(CheckCSourceCompiles)
 include(CheckFortranMPIFunctionExists)
-include(CheckFunctionExists)
 include(CheckMPIFunctionExists)
-include(CheckMPISymbolExists)
 
-
-###Run all feature tests that influence config.h
-#MPI-2 constants
-check_mpi_symbol_exists(MPI_STATUS_IGNORE HAVE_MPI_STATUS_IGNORE)
-check_mpi_symbol_exists(MPI_STATUSES_IGNORE HAVE_MPI_STATUSES_IGNORE)
-check_mpi_symbol_exists(MPI_F_STATUS_IGNORE HAVE_MPI_F_STATUS_IGNORE)
-check_mpi_symbol_exists(MPI_F_STATUSES_IGNORE HAVE_MPI_F_STATUSES_IGNORE)
 
 #MPI 2 Functionality
 check_mpi_function_exists(MPI_Init_thread HAVE_MPI_INIT_THREAD_C)
@@ -49,26 +39,3 @@ IF (CMAKE_Fortran_COMPILER_WORKS)
   check_fortran_mpi_function_exists(MPI_INIT_THREAD
     HAVE_MPI_INIT_THREAD_Fortran)
 ENDIF (CMAKE_Fortran_COMPILER_WORKS)
-
-check_mpi_function_exists(MPI_Type_create_indexed_block
-  HAVE_PMPI_TYPE_CREATE_INDEXED_BLOCK)
-
-# handle convert macros may be present
-##@TODO we also have feature tests for the other handles, we just didn't have the time to
-##handle them correctly in the existing stuff, so right now just what we use
-##@NOTE These tests are CPP as for MPICH2(?) the Request_c2f/f2c stuff is broken for
-##CPP (but nor for C); with that we see the issue there and do not use them for this MPI,
-##which is a good choice
-SET (HandleConvertMacros
-    MPI_Status
-    CACHE INTERNAL "All the handle convert macros"
-)
-
-FOREACH (type ${HandleConvertMacros})
-  string(TOUPPER "HAVE_${type}" variable)
-
-  check_mpi_function_exists(${type}_f2c "${variable}_F2C")
-  if (${${variable}_F2C})
-    check_mpi_function_exists(${type}_c2f "${variable}_C2F")
-  endif ()
-ENDFOREACH (type)
